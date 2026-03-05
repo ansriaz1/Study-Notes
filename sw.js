1c52bd2f23cb380d2646b2a676d40bab0d1f1987
ansriaz1 <ansriaz93@gmail.com>
2026-03-05 23:27:12 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,23 @@
-const cacheName="notes-app"
+const cacheName = "notes-app-v1";
 
-self.addEventListener("install",e=>{
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
 
-e.waitUntil(
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
 
-caches.open(cacheName).then(cache=>{
-
-return cache.addAll([
-
-"/",
-
-"index.html",
-
-"style.css",
-
-"app.js"
-
-])
-
-})
-
-)
-
-})
-
-self.addEventListener("fetch",e=>{
-
-e.respondWith(
-
-caches.match(e.request).then(res=>res || fetch(e.request))
-
-)
-
-})
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
